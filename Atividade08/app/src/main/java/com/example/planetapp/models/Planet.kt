881e3712280cba77a6ac7739b19344f2ef9a412a
Kelvyh <kelvyhallyson4@gmail.com>
2024-12-06 23:34:23 -0300

    adicionada atividade 08

---
+++ Atividade08/app/src/main/java/com/example/planetapp/models/Planet.kt
@@ -0,0 +1,107 @@
+package com.example.planetapp.models
+import com.example.planetapp.R
+
+data class Planet(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val galaxy: String,
+    val distanceFromSun: String,
+    val diameter: String,
+    val characteristics: String,
+    val imageRes: Int,
+    var isFavorite: Boolean = false
+)
+
+val planetList = listOf(
+    Planet(
+        id = 1,
+        name = "Earth",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "149.6 million km",
+        diameter = "12,742 km",
+        characteristics = "Supports life, has water and atmosphere.",
+        imageRes = R.drawable.terra
+    ),
+    Planet(
+        id = 2,
+        name = "Mercury",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "57.9 million km",
+        diameter = "4,880 km",
+        characteristics = "The closest planet to the Sun, has extreme temperature variations.",
+        imageRes = R.drawable.mercurio
+    ),
+    Planet(
+        id = 3,
+        name = "Venus",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "108.2 million km",
+        diameter = "12,104 km",
+        characteristics = "Similar size to Earth, but with a thick toxic atmosphere and extreme temperatures.",
+        imageRes = R.drawable.venus
+    ),
+    Planet(
+        id = 4,
+        name = "Mars",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "227.9 million km",
+        diameter = "6,779 km",
+        characteristics = "Known as the Red Planet, has the largest volcano and canyon in the solar system.",
+        imageRes = R.drawable.marte
+    ),
+    Planet(
+        id = 5,
+        name = "Jupiter",
+        type = "Gas Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "778.3 million km",
+        diameter = "139,820 km",
+        characteristics = "The largest planet, known for its Great Red Spot and numerous moons.",
+        imageRes = R.drawable.jupiter
+    ),
+    Planet(
+        id = 6,
+        name = "Saturn",
+        type = "Gas Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "1.4 billion km",
+        diameter = "116,460 km",
+        characteristics = "Famous for its stunning rings made of ice and rock.",
+        imageRes = R.drawable.saturno
+    ),
+    Planet(
+        id = 7,
+        name = "Uranus",
+        type = "Ice Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "2.9 billion km",
+        diameter = "50,724 km",
+        characteristics = "Known for its tilted axis, which causes extreme seasons.",
+        imageRes = R.drawable.urano
+    ),
+    Planet(
+        id = 8,
+        name = "Neptune",
+        type = "Ice Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "4.5 billion km",
+        diameter = "49,244 km",
+        characteristics = "Known for its deep blue color and strong winds.",
+        imageRes = R.drawable.netuno
+    ),
+    Planet(
+        id = 9,
+        name = "Pluto",
+        type = "Dwarf Planet",
+        galaxy = "Milky Way",
+        distanceFromSun = "5.9 billion km",
+        diameter = "2,377 km",
+        characteristics = "Once considered the ninth planet, now classified as a dwarf planet.",
+        imageRes = R.drawable.plutao
+    )
+)
